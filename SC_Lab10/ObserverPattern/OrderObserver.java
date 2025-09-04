3297a565b831ba3756a0f2f6489eb455e9ccefd8
chayaponsaenthavee <chayapon.sae@ku.th>
2025-09-05 00:23:42 +0700

    Delete SC_Lab10 directory

--- SC_Lab10/ObserverPattern/OrderObserver.java
+++
@@ -1,9 +0,0 @@
-package ObserverPattern;
-import DataModels.*;
-/*
- * Interface (Observer) สำหรับผู้สังเกตการณ์
- */
-
-public interface OrderObserver {
-    void update(Order order);
-}
--- SC_Lab10/StrategyPattern/DiscountStrategy.java
+++
@@ -1,8 +0,0 @@
-package StrategyPattern;
-import DataModels.*;
-/*
- * Interface สำหรับกลยุทธ์ส่วนลด
- */
-public interface DiscountStrategy {
-    double applyDiscount (Order order);
-}
